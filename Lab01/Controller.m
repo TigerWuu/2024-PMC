db634b189403a424d573b88101f790de62a7a9d7
TigerWuu <b06611032@ntu.edu.tw>
2024-03-29 20:21:50 +0800

    ADD: lab01 figures

--- Lab01/Controller.m
+++ Lab01/Controller.m
@@ -4,8 +4,10 @@ close all;
 
 %% initialization
 load("sys_est.mat");
+load("sys_hw.mat");
 load("Pa.mat");
 % sys_est = Pa;
+% sys_est = sys_hw;
 
 fs = 20000;
 Ts = 1/fs;
@@ -16,24 +18,44 @@ t= 0:Ts:1;
 r = 0.08*sin(2*pi*hz*t);
 lsim(sys_est, r, t);
 
-%% controller design : k controller
+%% controller design : P controller
 % spec ?
 rlocus(sys_est);
 zgrid;
 
 %% closed-loop : P controller
-kp = 1.5;
+kp = 0.9;
 sys_est_cls_P = feedback(sys_est*kp, 1);
 lsim(sys_est_cls_P, r, t);
 
-%% controller design : k controller
+%% controller design : PD controller
 % spec ?
 z = tf('z',Ts);
-sys_est_L = ((z-1)*sys_est)/(z*(1+kp*sys_est));
-rlocus(sys_est_L);
+sys_est_L_pd = ((z-1)*sys_est)/(z*(1+kp*sys_est));
+rlocus(sys_est_L_pd);
 zgrid;
 
 %% closed-loop : PD controller
-kd = 5.5;
-sys_est_cls_PD = feedback(sys_est_L*kd, 1);
+kd = 1;
+sys_est_cls_PD = feedback(sys_est_L_pd*kd, 1);
 lsim(sys_est_cls_PD, r, t);
+
+%% controller design : PID controller
+% spec ?
+z = tf('z',Ts);
+sys_est_L_pid = ((1-z^(-1))^(-1)*sys_est)/(z*(1+kp*sys_est+sys_est*kd*(1-z^(-1))));
+rlocus(sys_est_L_pid);
+zgrid;
+
+%% closed-loop : PID controller
+ki = 0.4;
+sys_est_cls_PID = feedback(sys_est_L_pid*ki, 1);
+lsim(sys_est_cls_PID, r, t);
+%% pid close bode
+plotoptions = bodeoptions;
+plotoptions.FreqUnits = 'Hz';   
+hold on;
+bodeplot(sys_est_cls_PD, plotoptions);
+bodeplot(sys_est, plotoptions);
+
+
--- Lab01/SystemID.m
+++ Lab01/SystemID.m
@@ -1,11 +1,20 @@
 clc;
 clear;
-
+close all;
 %% initialization
 fs = 20000;
 Ts = 1/fs;
-w = {10, 10000};
+w = {10*2*pi, 10000*2*pi}; % hz to rad/s because the unit of w is rad/s
 load("Pa.mat");
+plotoptions = bodeoptions;
+plotoptions.Grid = 'on';
+plotoptions.FreqUnits = 'Hz';   
+plotoptions.Title.String = 'Bode Plot (Complex Curve Fitting)';
+plotoptions.Title.FontSize = 15;
+plotoptions.Title.FontWeight = "bold";
+plotoptions.Xlabel.FontSize= 12;
+plotoptions.Ylabel.FontSize= 12;
+plotoptions.Ticklabel.FontSize= 10;
 
 %% concatenate all the u and y
 ufreq = [10, 20, 30, 50, 100, 200, 300, 500, 1000, 2000, 3000, 5000];
@@ -55,32 +64,58 @@ n = 5;
 [a,b] = complexfit(Gks(1:12)', n, ufreq, fs);
 a = cat(1,[1],a);
 sys_hw = tf(b',a',Ts);
-plotoptions = bodeoptions;
-plotoptions.Grid = 'on';
-plotoptions.FreqUnits = 'Hz';
-plotoptions.Title.String = 'Bode Plot of Transfer Function';
+
+plotoptions.Title.String = 'Bode Plot (Complex Curve Fitting)';
 % plotoptions.PhaseWrapping = "on";
 % plotoptions.PhaseWrappingBranch = -360;
-
 bodeplot(sys_hw, w, plotoptions);
+save("sys_hw.mat", "sys_hw");
+% [mag,phase,wout] = bode(sys_hw, w);
+% % magnitude
+% subplot(211)
+% semilogx(wout,mag2db(abs(squeeze(mag))),'-','linewidth',1.5); hold on;
+% grid on; grid minor;
+% ylabel('Magnitude [dB]','interpreter','latex','fontsize',12)
+% title('Bode Plot (Complex Curve Fitting)','interpreter','latex','fontsize',20);
+% % phase
+% subplot(212)
+% semilogx(wout,squeeze(phase),'-','linewidth',1.5); hold on;
+% grid on; grid minor;
+% ylabel('Phase [deg]','interpreter','latex','fontsize',12)
+% xlabel('Frequency [Hz]','interpreter','latex','fontsize',12)
+
+% legend("frd",'interpreter','latex','fontsize',12,'location','best')
 %% complex curve fitting by matlab toolbox
 figure();
-sys_frd=frd(Gks,ufreq);
-model=idtf([NaN NaN NaN NaN NaN],[1 NaN NaN NaN NaN NaN]);
-model.Structure.den.Minimum=0;
-sys_est = tfest(sys_frd,model);
-sys_est = c2d(sys_est,Ts); % why??
+sys_frd=frd(Gks,ufreq,Ts,'FrequencyUnit','Hz');
+model=idtf([NaN NaN 0 0 0],[1 NaN NaN NaN NaN NaN]);
+sys_est = tfest(sys_frd,model,'Ts', Ts);
+% sys_est = c2d(sys_est,Ts); % why??
 
-plotoptions = bodeoptions;
-plotoptions.Grid = 'on';
-plotoptions.FreqUnits = 'Hz';
-plotoptions.Title.String = 'Bode Plot of Transfer Function';
+plotoptions.Title.String = 'Bode Plot (tfest)';
 % plotoptions.PhaseWrapping = "on";
 % plotoptions.PhaseWrappingBranch = -360;
 
-bodeplot(sys_est, w, plotoptions);
+
+[mag,phase,wout] = bode(sys_est, w); % the unit of wout is rad/s
+wout = wout/2/pi; % rad/s to Hz
+% magnitude
+subplot(211)
+semilogx(wout,mag2db(abs(squeeze(mag))),'-','linewidth',1.5); hold on;
+semilogx(ufreq,mag2db(abs(Gks)),'.','MarkerSize',10);
+grid on; grid minor;
+ylabel('Magnitude [dB]','interpreter','latex','fontsize',12)
+title('Bode Plot (Complex Curve Fitting)','interpreter','latex','fontsize',20);
+% phase
+subplot(212)
+semilogx(wout,squeeze(phase),'-','linewidth',1.5); hold on;
+semilogx(ufreq,rad2deg(angle(Gks)),'.','MarkerSize',10);
+grid on; grid minor;
+ylabel('Phase [deg]','interpreter','latex','fontsize',12)
+xlabel('Frequency [Hz]','interpreter','latex','fontsize',12)
+% bodeplot(sys_est, w, plotoptions);
 save("sys_est.mat", "sys_est");
-%% true tf
+%% Paper's tf
 figure();
 numerator = [-0.001647,0.05631,0,0,0];
 denominator = [1,-2.804,3.599,-2.594,1.042,-0.1882];
@@ -88,16 +123,18 @@ denominator = [1,-2.804,3.599,-2.594,1.042,-0.1882];
 sys_true_d = tf(numerator,denominator,Ts);
 sys_true_c = d2c(sys_true_d);
 
-plotoptions = bodeoptions;
-plotoptions.Grid = 'on';
-% plotoptions.FreqUnits = 'Hz';
-plotoptions.Title.String = 'Bode Plot of Transfer Function';
+plotoptions.Title.String = 'Bode Plot (Paper)';
 % plotoptions.PhaseWrapping = "on";
 % plotoptions.PhaseWrappingBranch = -360;
 
 bodeplot(sys_true_d, w, plotoptions);
 
-%% compare
+%% TA's tf
+figure();
+plotoptions.Title.String = 'Bode Plot (TA)';
+bodeplot(Pa, w, plotoptions);
+
+%% compare : frequency response
 % true vs. toolbox
 figure();
 hold on;
@@ -123,9 +160,57 @@ legend;
 figure();
 hold on;
 bodeplot(Pa, w, plotoptions);
-bodeplot(sys_est, w, plotoptions);
+bodeplot(sys_true_d, w, plotoptions);
 legend;
 
+% Ta vs.hw3
+figure();
+plotoptions.Title.String = 'Bode Plot : TA vs. CCF';
+hold on;
+bodeplot(Pa, w, plotoptions);
+bodeplot(sys_hw, w, plotoptions);
+legend;
+
+
+% Ta vs.hw3
+figure();
+plotoptions.Title.String = 'Bode Plot : TA vs. tfest';
+hold on;
+bodeplot(Pa, w, plotoptions);
+bodeplot(sys_est, w, plotoptions);
+legend;
 % [Gk , f] = frf_spa(y,u,Ts,"H1",true);
 % plot(t,y,t,u);
 % plot(u)
+
+
+%% compare : step response
+% Ta vs. tfest
+figure();
+hold on;
+tf = 0.001;
+[y,t] = step(Pa,tf);
+plot(t, squeeze(y), 'LineWidth',2);
+[y,t] = step(sys_est,tf);
+plot(t, squeeze(y), 'LineWidth',2)
+
+grid on; grid minor;
+ylabel('Amplitude[deg]','interpreter','latex','fontsize',12);
+xlabel('Time[s]','interpreter','latex','fontsize',12);
+title('Step: Ta vs. tfest','interpreter','latex','FontWeight','bold','fontsize',15);
+legend("Ta's","tfest",'interpreter','latex','fontsize',12,'location','best');
+
+% Ta vs. ccf
+figure();
+hold on;
+tf = 6*0.001;
+[y,t] = step(Pa,tf);
+plot(t, squeeze(y), 'LineWidth',2);
+[y,t] = step(sys_hw,tf);
+plot(t, squeeze(y), 'LineWidth',2)
+
+grid on; grid minor;
+ylabel('Amplitude[deg]','interpreter','latex','fontsize',12);
+xlabel('Time[s]','interpreter','latex','fontsize',12);
+title('Step: Ta vs. CCF','interpreter','latex','FontWeight','bold','fontsize',15);
+legend("Ta's","CCF",'interpreter','latex','fontsize',12,'location','best');
