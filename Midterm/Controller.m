60dc20ace794f23ac9f5eb32bf36a0d2c709ed0e
TigerWuu <b06611032@ntu.edu.tw>
2024-04-28 12:01:47 +0800

    ADD:sensitivity analysis

--- Midterm/Controller.m
+++ Midterm/Controller.m
@@ -1,6 +1,16 @@
 clc;
 clear;
 close all;
+%%
+plotoptions = bodeoptions;
+plotoptions.Grid = 'on';
+plotoptions.FreqUnits = 'Hz';   
+plotoptions.Title.String = 'Bode Plot (Complex Curve Fitting)';
+plotoptions.Title.FontSize = 15;
+plotoptions.Title.FontWeight = "bold";
+plotoptions.Xlabel.FontSize= 12;
+plotoptions.Ylabel.FontSize= 12;
+plotoptions.Ticklabel.FontSize= 10;
 
 %% initialization
 load("sys_est.mat");
@@ -20,6 +30,7 @@ sys_est_ss = ss(A,B,C,D,Ts);
 % poleplacement
 poles = [0.5, 0.6, 0.7, 0.8, 0.9];
 poles = [0.5, 0.51, 0.52, 0.53, 0.54]; % how to choose
+% poles = [0.5, 0.6, 0.7, 0.71, 0.72]; % how to choose
 % poles = [0.1, 0.2, 0.3, 0.4, 0.5];
 % poles = [0.1, 0.11, 0.12, 0.13, 0.14];
 % L = [0;0;0;0;0];
@@ -28,7 +39,7 @@ L = place(A',C',poles)';
 
 eig(A-L*C)
 %% design K (LQR)
-Q = 20*eye(5);
+Q = 1*eye(5);
 R = 1;
 N = zeros(5,1);
 [K,S,e] = dlqr(A,B,Q,R,N);
@@ -43,8 +54,8 @@ K = place(A,B,poles);
 
 eig(A-B*K)
 %% design M 
-w0 = 400/fs*2*pi;
-Ma = [0 , 1 ; -1, 2*cos(w0*Ts)];
+w0 = 400/fs*2*pi; % to normalized frequency(but why?
+Ma = [0 , 1 ; -1, 2*cos(w0)];
 Mb = [0;1];
 Mc = [1,0];
 Md = 0;
@@ -61,6 +72,25 @@ poles = [0.85, 0.8, 0.1, 0.11, 0.15, -0.45, -0.4];
 Kaug = place(A_aug , B_aug , poles);
 K = Kaug(1:length(A));
 Km = Kaug(length(A)+1:end);
+
+%% Loop gain analysis (with internal model)
+Lg = ss(A-L*C-B*K,L,K,0,Ts)*sys_est_ss+ ... 
+    ss(A-L*C-B*K,L,K,0,Ts)*sys_est_ss*ss(Ma,Mb,-Km,0,Ts)- ...
+    sys_est_ss*ss(Ma,Mb,-Km,0,Ts);
+
+% bode(Lg);
+% sensitivity analysis
+S = feedback(1,Lg);
+hold on;
+bodeplot(S, plotoptions);
+%% Loop gain analysis (without internal model)
+Lg = ss(A-L*C-B*K,L,K,0,Ts)*sys_est_ss;
+
+
+% bode(Lg);
+% sensitivity analysis
+S = feedback(1,Lg);
+bodeplot(S, plotoptions);
 %% save designed parameters
 writematrix(L,'./design/L.xls');
 writematrix(K,'./design/K.xls');
