421cce450d6c3618b3418cb037345cb32a3625cb
TigerWuu <b06611032@ntu.edu.tw>
2024-06-16 00:58:38 +0800

    Mod:Midterm, finish the report

--- Midterm/Controller.m
+++ Midterm/Controller.m
@@ -5,7 +5,7 @@ close all;
 plotoptions = bodeoptions;
 plotoptions.Grid = 'on';
 plotoptions.FreqUnits = 'Hz';   
-plotoptions.Title.String = 'Bode Plot (Complex Curve Fitting)';
+plotoptions.Title.String = 'Sensitivity';
 plotoptions.Title.FontSize = 15;
 plotoptions.Title.FontWeight = "bold";
 plotoptions.Xlabel.FontSize= 12;
@@ -14,6 +14,9 @@ plotoptions.Ticklabel.FontSize= 10;
 
 %% initialization
 load("sys_est.mat");
+% load("Pa.mat");
+% sys_est = Pa;
+
 fs = 20000;
 Ts = 1/fs;
 
@@ -28,10 +31,12 @@ sys_est_ss = ss(A,B,C,D,Ts);
 % zgrid;
 %% design L
 % poleplacement
-poles = [0.5, 0.6, 0.7, 0.8, 0.9];
-poles = [0.5, 0.51, 0.52, 0.53, 0.54]; % how to choose
-% poles = [0.5, 0.6, 0.7, 0.71, 0.72]; % how to choose
+% poles = [0.9, 0.91, 0.92, 0.93, 0.94]; % bad
+% poles = [0.5, 0.6, 0.7, 0.8, 0.9];
+% poles = [0.5, 0.51, 0.52, 0.53, 0.54]; % how to choose
+poles = [0.5, 0.55, 0.62, 0.63, 0.6]; % how to choose
 % poles = [0.1, 0.2, 0.3, 0.4, 0.5];
+% poles = [0.3, 0.31, 0.32, 0.33, 0.34];
 % poles = [0.1, 0.11, 0.12, 0.13, 0.14];
 % L = [0;0;0;0;0];
 L = place(A',C',poles)';
@@ -49,8 +54,10 @@ eig(A-B*K)
 % poles = [0.5, 0.6, 0.7, 0.8, 0.9];
 % poles = [0.9, 0.91, 0.92, 0.93, 0.94];
 poles = [0.85, 0.8, 0.1, 0.11, 0.15];
+% poles = [0.85, 0.8, 0.7, 0.65, 0.6];
 K = place(A,B,poles);
 % K = [0 ,0, 0, 0, 0];
+% writematrix(K,'./design/K_pp.csv');
 
 eig(A-B*K)
 %% design M 
@@ -68,7 +75,8 @@ A_aug = [A , zeros(length(A),length(Ma));Mb*C, Ma];
 B_aug = [B;zeros(length(Ma),1)];
 C_aug = [C , zeros(1, length(Ma))];
 
-poles = [0.85, 0.8, 0.1, 0.11, 0.15, -0.45, -0.4];
+% poles = [0.85, 0.8, 0.1, 0.11, 0.15, -0.45, -0.4]; % aggressive
+poles = [0.85, 0.8, 0.7, 0.65, 0.6, -0.6, -0.55]; % non-aggressive
 Kaug = place(A_aug , B_aug , poles);
 K = Kaug(1:length(A));
 Km = Kaug(length(A)+1:end);
@@ -92,5 +100,23 @@ Lg = ss(A-L*C-B*K,L,K,0,Ts)*sys_est_ss;
 S = feedback(1,Lg);
 bodeplot(S, plotoptions);
 %% save designed parameters
-writematrix(L,'./design/L.xls');
-writematrix(K,'./design/K.xls');
+% writematrix(A,'./design/A.csv');
+% writematrix(B,'./design/B.csv');
+% writematrix(C,'./design/C.csv');
+% 
+% % writematrix(L,'./design/L.csv');
+% % writematrix(K,'./design/K.csv');
+% % 
+% % writematrix(Ma,'./design/Ma.csv');
+% % writematrix(Mb,'./design/Mb.csv');
+% % writematrix(-Km,'./design/Mc.csv');
+% 
+% writematrix(L,'./design/La.csv');
+% writematrix(K,'./design/Ka.csv');
+% 
+% writematrix(Ma,'./design/Maa.csv');
+% writematrix(Mb,'./design/Mba.csv');
+% writematrix(-Km,'./design/Mca.csv');
+
+
+
--- Midterm/data_visualization.m
+++ Midterm/data_visualization.m
@@ -1,5 +1,6 @@
-% uav_visualization 
 close all;
+
+%%
 t = out.tout;
 
 stepRespons = get(out.logsout, 'step').Values.Data;
@@ -17,7 +18,24 @@ titlefont = 20;
 tickfont = 18;
 legendfont = 16;
 
-%% step response
+%% compare y_real vs. y_sim
+filename = './experiments/0325/Group5-2024-03-25-11-30-34-StfbObsv_only-0.20deg-step-10.0s.csv';
+data = readmatrix(filename)';
+save("experiment.mat", "data");
+
+t_r = data(1,:);
+step_y_r = data(4,:);
+
+figure();
+plot(t_r, step_y_r, '-','linewidth',1.5); hold on;
+plot(t, step_y_hat, t, step_r,'-','linewidth',1.5); 
+xlabel('Times[s]','interpreter','latex','fontsize',tickfont);
+ylabel('Magnitude[m]','interpreter','latex','fontsize',tickfont);
+title('Step response', 'fontsize',titlefont);
+legend("$y_{experiment}$","$\hat{y}$","$r$",'interpreter','latex','fontsize',legendfont,'location','best')
+grid on; grid minor;
+
+%% step response (simulation)
 figure();
 plot(t, step_r, t, step_y, t, step_y_hat,'-','linewidth',1.5); hold on;
 xlabel('Times[s]','interpreter','latex','fontsize',tickfont);
@@ -27,7 +45,7 @@ legend("$r$","$y$","$\hat{y}$",'interpreter','latex','fontsize',legendfont,'loca
 grid on; grid minor;
 
 
-%% sinewave tracking
+%% sinewave tracking (simulation)
 figure();
 plot(t, sine_r, t, sine_y, t, sine_y_hat,'-','linewidth',1.5); hold on;
 xlabel('Times[s]','interpreter','latex','fontsize',tickfont);
@@ -36,17 +54,50 @@ title('400 Hz Sinewave tracking', 'fontsize',titlefont);
 legend("$r$","$y$","$\hat{y}$",'interpreter','latex','fontsize',legendfont,'location','best')
 grid on; grid minor;
 
-% figure();
-% colors = colormap(jet(length(t))); 
-% for i = 1:100:length(t)
-%   plot(x(i), y(i),'.','linewidth',1.5,"color",colors(i,:)); hold on;
-% end
-% c=colorbar('Ticks',linspace(0,1,11),'TickLabels',linspace(0,t(end),11));
-% c.Label.String = 'Time[s]';
-% 
-% % plot(x, y,'-','linewidth',1.5);
-% xlabel('x[m]','interpreter','latex','fontsize',12);
-% ylabel('y[m]','interpreter','latex','fontsize',12);
-% title('Position[ENU]');
-% legend("2D position(XY)",'interpreter','latex','fontsize',12,'location','best')
-% grid on; grid minor;
+%% step response (experiment)
+filename = './experiments/0520/Group5-2024-05-20-11-11-32-StfbObsv_only-0.20deg-step-10.0s_Kpp.csv';
+data = readmatrix(filename);
+t = data(:,1);
+step_r = data(:,3);
+step_y = data(:,4);
+step_y_hat = data(:,5);
+
+figure();
+plot( t, step_y, t, step_y_hat,t, step_r,'-','linewidth',1.5); hold on;
+xlabel('Times[s]','interpreter','latex','fontsize',tickfont);
+ylabel('Magnitude[m]','interpreter','latex','fontsize',tickfont);
+title('Step response tracking', 'fontsize',titlefont);
+legend("$y$","$\hat{y}$","$r$",'interpreter','latex','fontsize',legendfont,'location','best')
+grid on; grid minor;
+
+%% sinewave tracking - nonaggressive poles (experiment)
+filename = './experiments/0520/Group5-2024-05-20-11-13-57-StfbObsv_withIMP-0.20deg-400HzSine-10.0s_noaggressive.csv';
+data = readmatrix(filename);
+t = data(:,1);
+sine_r = data(:,3);
+sine_y = data(:,4);
+sine_y_hat = data(:,5);
+
+figure();
+plot(t, sine_r, t, sine_y, t, sine_y_hat,'-','linewidth',1.5); hold on;
+xlabel('Times[s]','interpreter','latex','fontsize',tickfont);
+ylabel('Magnitude[m]','interpreter','latex','fontsize',tickfont);
+title('400Hz sinewave tracking', 'fontsize',titlefont);
+legend("$r$","$y$","$\hat{y}$",'interpreter','latex','fontsize',legendfont,'location','best')
+grid on; grid minor;
+
+%% sinewave tracking - aggressive poles (experiment)
+filename = './experiments/0521/Group5-2024-05-21-10-09-05-StfbObsv_withIMP-0.20deg-400HzSine-10.0s-aggressive.csv';
+data = readmatrix(filename);
+t = data(:,1);
+sine_r = data(:,3);
+sine_y_f = data(:,4);
+sine_y_hat_f = data(:,5);
+
+figure();
+plot(t, sine_r, t, sine_y_f, t, sine_y_hat_f,'-','linewidth',1.5); hold on;
+xlabel('Times[s]','interpreter','latex','fontsize',tickfont);
+ylabel('Magnitude[m]','interpreter','latex','fontsize',tickfont);
+title('400Hz sinewave tracking', 'fontsize',titlefont);
+legend("$r$","$y$","$\hat{y}$",'interpreter','latex','fontsize',legendfont,'location','best')
+grid on; grid minor;
